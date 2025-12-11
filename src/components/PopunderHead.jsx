0a7dde5f9093f5db7202ff90ba5ee5bd08132813
Samuel Kimin <samuel.kimin0@gmail.com>
2025-12-11 22:53:42 +0700

    Ad Disabled

--- src/components/PopunderHead.jsx
+++ src/components/PopunderHead.jsx
@@ -4,10 +4,10 @@ import { HelmetProvider } from "react-helmet-async";
 export default function PopunderHead() {
   return (
     <Helmet>
-      <script
+      {/* <script
         type="text/javascript"
         src="//pl28237572.effectivegatecpm.com/20/96/f0/2096f072aaf88cd447ba8b516c60a8db.js"
-      ></script>
+      ></script> */}
     </Helmet>
   );
 }
