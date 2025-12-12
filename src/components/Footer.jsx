99ffdff6a75af74868ea9ce6d00e0dd2826703d1
Samuel Kimin <samuel.kimin0@gmail.com>
2025-12-12 15:30:57 +0700

    UI Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,27 @@
 import "./Footer.css";
+import XLogo from '../assets/XLogo.png';
 
-function About() {
-  return (
-    <div className="footer">
-      <p classname='cftitle' id='cft'>uFlix - Box Office Movies</p>
-      <p>English - US</p>
-      <p classname='cftitle' id="cfts">REFERENCE</p>
-      <p id="cfds">The Movie Database (TMDB)</p>
-      <p className="cftitle" id="cfts">LEGAL</p>
-      <a href="https://www.themoviedb.org/privacy-policy" id="cfds" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
-    </div>
-  );
+function Footer() {
+return ( <div className="footer"> <p className='cftitle' id='cft'>uFlix - Box Office Movies</p>
+
+
+  <div className="follow-us">
+    <span>Follow us on :</span>
+    <a href="https://x.com/uflix_space" target="_blank" rel="noopener noreferrer" className="social-icon">
+      <img src={XLogo} alt="X Logo" />
+    </a>
+    <a href="https://x.com/uflix_space" id="cfds" target="_blank" rel="noopener noreferrer">@uflix_space</a>
+  </div>
+
+  <p>English - US</p>
+  <p className='cftitle' id="cfts">REFERENCE</p>
+  <p id="cfds">The Movie Database (TMDB)</p>
+  <p className="cftitle" id="cfts">LEGAL</p>
+  <a href="https://www.themoviedb.org/privacy-policy" id="cfds" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+</div>
+
+
+);
 }
 
-export default About;
+export default Footer;
